787bf05cb497e758d6dbebcc918756229b54f696
Daniel Rötzer <roetzer.daniel@gmail.com>
2017-03-20 14:23:50 +0100

    Final 2

--- public/js/rtvs.js
+++ public/js/rtvs.js
@@ -57,14 +57,3 @@ $(document).ready(function () {
         }
     });
 });
-/**
- * Created by Dani on 16.03.2017.
- */
-
-$(document).ready(function () {
-    const socket = io.connect();
-
-    socket.on('answerUpdate', function (data){
-        location.reload();
-    });
-});
